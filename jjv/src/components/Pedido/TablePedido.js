19e49b65e5b074356bd4eb109c6843e6ebb60540
jaovy05 <joaovitorg.silva050@gmail.com>
2024-07-09 15:16:14 -0300

    oculta tbm o relatorio e o cadastrar modelo

--- jjv/src/components/Pedido/TablePedido.js
+++ jjv/src/components/Pedido/TablePedido.js
@@ -8,11 +8,6 @@ import ModalClose from '@mui/joy/ModalClose';
 import Modal from '@mui/joy/Modal';
 import SimpleAlert from '../Alerts/SuccessAlert';
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> a639927 (criando relatorio, corte de peças e modelo)
 function TablePedido() {
   const [pedidos, setPedidos] = React.useState([]);
   const [pedidoSelecionado, setPedidoSelecionado] = React.useState(null);
@@ -20,10 +15,6 @@ function TablePedido() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a639927 (criando relatorio, corte de peças e modelo)
   React.useEffect(() => {
     async function fetchPedidos() {
       try {
